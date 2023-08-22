8799466527042725036c650a278c00571e90cf74
Arya Priema <99aryaprima@gmail.com>
2023-08-22 22:17:32 +0700

    add editor config

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,24 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Manrope } from 'next/font/google'
+import "./globals.css";
+import type { Metadata } from "next";
+import { Manrope } from "next/font/google";
 
-const manrope = Manrope({ subsets: ['latin'] })
+const manrope = Manrope({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Image Optimization',
-}
+    title: "Image Optimization",
+};
 
 export default function RootLayout({
-  children,
+    children,
 }: {
-  children: React.ReactNode
+    children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <body className={manrope.className}>
-        <main>
-          {children}
-        </main>
-      </body>
-    </html>
-  )
+    return (
+        <html lang="en">
+            <body className={manrope.className}>
+                <main>{children}</main>
+            </body>
+        </html>
+    );
 }
+
