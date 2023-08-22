d27f214c34cd282f0da0ec8fe529595d05511fd7
Arya Priema <99aryaprima@gmail.com>
2023-08-22 22:17:03 +0700

    clean up folder, or files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import React from 'react'
-
 const HomePage = () => {
   return (
-    <div>HomePage</div>
-  )
-}
+    <div>
+      <h1>Home Page</h1>
+    </div>
+  );
+};
 
-export default HomePage
+export default HomePage;
